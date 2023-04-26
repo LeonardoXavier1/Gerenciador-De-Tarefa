35e927c52c2e7c178cb961c325cc27e06c507c54
LeonardoXavier1 <leonardomx500@gmail.com>
2023-04-26 11:55:06 -0300

    gerenciador de tarefa

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+        <title>Gerenciador de Tarefa</title>
+</head>
+<body>
+        
+        <div class="container">
+                <div class="header">
+                        <h1>Gerenciador de Tarefa</h1>
+                </div>
+
+                <div class="form">
+                      <form action="" method="get">
+                        <label for="task_name"><label>
+                        <input type="text" name="task_name" placeholder="Nome da Tarefa:">
+                        <button type="submit">Cadastrar</button>
+                      </form>
+                </div>
+
+                <div class="separator">
+                </div>
+                
+                <div class="list-tasks">
+                        <ul>
+                                <li>Tarefa 1</li>
+                                <li>Tarefa 2</li>
+                                <li>Tarefa 3</li>
+                        </ul>
+                </div>
+
+                <footer><p  class ="escritas" > Site produzido por <a  href =" https://github.com/LeonardoXavier1 " target =" _blank "> Leonardo Matheus Xavier Vieira </a> 🔥 </p>
+</footer>
+                
+                
+        </div>
+
+</body>
+</html>
